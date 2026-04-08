cdda04c668b08868f7a2e0fe2d26010f03b722b1
Donchev, Kaloyan (EXTERN: MS) <kaloyan.donchev@car-digital.eu>
2026-04-08 11:30:58 +0300

    change uuid import

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,10 @@
 import { NextRequest, NextResponse } from "next/server"
-import { v4 as uuidv4 } from "uuid"
 
 export function middleware(request: NextRequest) {
   const response = NextResponse.next()
 
   if (!request.cookies.get("session_id")) {
-    response.cookies.set("session_id", uuidv4(), {
+    response.cookies.set("session_id", crypto.randomUUID(), {
       maxAge: 60 * 60 * 24 * 365,
       httpOnly: true,
     })
