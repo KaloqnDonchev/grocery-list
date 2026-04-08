94a32ee7c5b882351e3ef248806e3e4b77c7780b
Donchev, Kaloyan (EXTERN: MS) <kaloyan.donchev@car-digital.eu>
2026-04-08 11:15:15 +0300

    add force dynamic for refresh fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import { getItems } from "./actions"
 import GroceryList from "./GroceryList";
 
+export const dynamic = "force-dynamic";
+
 export default async function Home() {
   const groceryList = await getItems();
   return <GroceryList groceryList={groceryList} />;
